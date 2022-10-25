c31260d7e28a1b876175608b03283ae37470b4bc
Nassim <19241056@buaa.edu.cn>
2022-10-25 22:44:38 +0800

    send verify code

---
+++ ms-diners/src/main/java/com/imooc/diners/controller/SendVerifyCodeController.java
@@ -0,0 +1,34 @@
+package com.imooc.diners.controller;
+
+import com.imooc.commons.model.domain.ResultInfo;
+import com.imooc.commons.utils.ResultInfoUtil;
+import com.imooc.diners.service.SendVerifyCodeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 发送验证码的控制层
+ */
+@RestController
+public class SendVerifyCodeController {
+    @Resource
+    private SendVerifyCodeService sendVerifyCodeService;
+
+    @Resource
+    private HttpServletRequest request;
+
+    /**
+     * 发送验证码
+     * @return
+     */
+    @GetMapping("send")
+    public ResultInfo send(String phone){
+        sendVerifyCodeService.send(phone);
+        return ResultInfoUtil.buildSuccess("发送成功",request.getServletPath());
+    }
+
+
+}
--- ms-diners/src/main/java/com/imooc/diners/service/SendVerifyCodeService.java
+++ ms-diners/src/main/java/com/imooc/diners/service/SendVerifyCodeService.java
@@ -1,7 +1,56 @@
 package com.imooc.diners.service;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import com.imooc.commons.constant.RedisKeyConstant;
+import com.imooc.commons.utils.AssertUtil;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
 /**
- *
+ * 发送验证码的业务逻辑层
  */
+@Service
 public class SendVerifyCodeService {
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
+
+    /**
+     * 发送验证码
+     *
+     * @param phone
+     */
+    public void send(String phone){
+        // 检查非空
+        AssertUtil.isNotEmpty(phone,"手机号不能为空");
+        // 根据手机号查询是否已生成验证码
+        if (!checkCodeIsExpired(phone)){
+            // 已生成直接返回
+            return;
+        }
+        // 未生成随机生成6位验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 调用短信服务发送短信这里需要调运云服务，这里不再写，假设已经发送
+        // 发送成功，将验证码保存到redis，失效时间60s
+        String key = RedisKeyConstant.verify_code.getKey() + phone;
+        redisTemplate.opsForValue().set(key,code,60, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 根据手机号查询是否已经生成验证码,未生成返回true，已生成返回false
+     * @param phone
+     * @return
+     */
+    private boolean checkCodeIsExpired(String phone){
+        String key = RedisKeyConstant.verify_code.getKey() + phone;
+        String code = redisTemplate.opsForValue().get(key);
+        if (StrUtil.isBlank(code)) {
+            return true;
+        }
+        return false;
+    }
 }
